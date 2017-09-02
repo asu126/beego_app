9839679f38c5432fa483db2cb5729df7d54d7459
33 <suguojun126@163.com>
2017-09-02 10:44:13 +0800

    change database init

---
+++ lib/db/database.go
@@ -0,0 +1,99 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func InitDb() {
+	createdb()
+	Connect()
+}
+
+func Connect() {
+	var dsn string
+	db_type := beego.AppConfig.String("db_type")
+	db_host := beego.AppConfig.String("db_host")
+	db_port := beego.AppConfig.String("db_port")
+	db_user := beego.AppConfig.String("db_user")
+	db_pass := beego.AppConfig.String("db_pass")
+	db_name := beego.AppConfig.String("db_name")
+	db_path := beego.AppConfig.String("db_path")
+	db_sslmode := beego.AppConfig.String("db_sslmode")
+	switch db_type {
+	case "mysql":
+		// 注册驱动
+		orm.RegisterDriver("mysql", orm.DRMySQL)
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", db_user, db_pass, db_host, db_port, db_name)
+		break
+	case "postgres":
+		orm.RegisterDriver("postgres", orm.DRPostgres)
+		dsn = fmt.Sprintf("dbname=%s host=%s  user=%s  password=%s  port=%s  sslmode=%s", db_name, db_host, db_user, db_pass, db_port, db_sslmode)
+	case "sqlite3":
+		orm.RegisterDriver("sqlite3", orm.DRSqlite)
+		if db_path == "" {
+			db_path = "./"
+		}
+		dsn = fmt.Sprintf("%s%s.db", db_path, db_name)
+		break
+	default:
+		beego.Critical("Database driver is not allowed:", db_type)
+	}
+	// 备注：此处第一个参数必须设置为“default”（因为我现在只有一个数据库），否则编译报错说：必须有一个注册DB的别名为 default
+	orm.RegisterDataBase("default", db_type, dsn)
+}
+
+//创建数据库
+func createdb() {
+
+	db_type := beego.AppConfig.String("db_type")
+	db_host := beego.AppConfig.String("db_host")
+	db_port := beego.AppConfig.String("db_port")
+	db_user := beego.AppConfig.String("db_user")
+	db_pass := beego.AppConfig.String("db_pass")
+	db_name := beego.AppConfig.String("db_name")
+	db_path := beego.AppConfig.String("db_path")
+	db_sslmode := beego.AppConfig.String("db_sslmode")
+
+	var dsn string
+	var sqlstring string
+	switch db_type {
+	case "mysql":
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/?charset=utf8", db_user, db_pass, db_host, db_port)
+		sqlstring = fmt.Sprintf("CREATE DATABASE  if not exists `%s` CHARSET utf8 COLLATE utf8_general_ci", db_name)
+		break
+	case "postgres":
+		dsn = fmt.Sprintf("host=%s  user=%s  password=%s  port=%s  sslmode=%s", db_host, db_user, db_pass, db_port, db_sslmode)
+		sqlstring = fmt.Sprintf("CREATE DATABASE %s", db_name)
+		break
+	case "sqlite3":
+		if db_path == "" {
+			db_path = "./"
+		}
+		dsn = fmt.Sprintf("%s%s.db", db_path, db_name)
+		os.Remove(dsn)
+		sqlstring = "create table init (n varchar(32));drop table init;"
+		break
+	default:
+		beego.Critical("Database driver is not allowed:", db_type)
+	}
+	db, err := sql.Open(db_type, dsn)
+	if err != nil {
+		panic(err.Error())
+	}
+	r, err := db.Exec(sqlstring)
+	if err != nil {
+		log.Println(err)
+		log.Println(r)
+	} else {
+		log.Println("Database ", db_name, " created")
+	}
+	defer db.Close()
+
+}
--- main.go
+++ main.go
@@ -5,17 +5,13 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 
+	db "app/lib/db"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
-	// 注册驱动
-	orm.RegisterDriver("mysql", orm.DRMySQL)
-	// 注册默认数据库
-	// 我的mysql的root用户密码为tom，打算把数据表建立在名为test数据库里
-	// 备注：此处第一个参数必须设置为“default”（因为我现在只有一个数据库），否则编译报错说：必须有一个注册DB的别名为 default
-	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/orm_test?charset=utf8", 30)
+	db.InitDb()
 
 	// degug
 	orm.Debug = true
